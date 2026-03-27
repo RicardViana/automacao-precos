3af36739c5fa0d5eb12717e188a4b8baf7647e95
Ricardo Viana <ricardviana1@gmail.com>
2026-03-27 08:21:27 -0300

    Ajuste doheaders

--- automacao.py
+++ automacao.py
@@ -151,10 +151,12 @@ def obter_preco_atual(url, loja):
 
     # Configuração do cabecalho para requisicao
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Accept-Language": "pt-BR,pt;q=0.9"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Referer": "https://www.google.com/"
     }
-    
+
     try:
         # Leitura dos dados do site
         resposta = requests.get(url, headers=headers)
